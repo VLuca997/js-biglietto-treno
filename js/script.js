75f4ee932f73085d63328b92b0071dd78659b528
VLuca997 <kulertoast997@gmail.com>
2023-06-12 17:48:38 +0200

    Correzione finale

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 // Chiedere all'utente il numero di chilometri da percorrere
-const km = parseFloat(prompt("Inserisci il numero di chilometri da percorrere:"));
+var km = parseFloat(prompt("Inserisci il numero di chilometri da percorrere:"));
 
 
 
 
 // Chiedere all'utente l'età del passeggero
-const age = parseInt(prompt("Inserisci l'età del passeggero:"));
+var age = parseInt(prompt("Inserisci l'età del passeggero:"));
 
 
 
 
 // Calcolare il prezzo del biglietto
-const ticketPrice = km * 0.21; // Prezzo del biglietto in base ai chilometri
+var ticketPrice = km * 0.21; // Prezzo del biglietto in base ai chilometri
 
 
 
 
 // Sconto in base all'età
-const discount = 0; // Valore percentuale dello sconto
+var discount = 0; // Valore percentuale dello sconto
 
 if (age < 18) {
   discount = 20; // Sconto del 20% per i minorenni
@@ -32,13 +32,13 @@ else if (age >= 65) {
 
 
 // Formattare il prezzo del biglietto
-const formattedPrice = ticketPrice.toFixed(2) + " €"; // Prezzo del biglietto con due decimali
+var formattedPrice = ticketPrice.toFixed(2) + " €"; // Prezzo del biglietto con due decimali
 
 
 
 
 // Creare il messaggio finale da visualizzare
-const message = "Numero di chilometri: " + km + " Km Da Percorrere" + "<br>" +
+var message = "Numero di chilometri: " + km + " Km Da Percorrere" + "<br>" +
                 "Prezzo del biglietto: " + formattedPrice + "<br>" +
                 "Età del passeggero: " + age + "<br>" +
                 "Sconto: " + discount + "%";
